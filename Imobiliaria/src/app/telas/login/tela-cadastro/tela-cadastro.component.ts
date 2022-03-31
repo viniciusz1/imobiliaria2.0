3fbab8618184f6428b75f107df3160236214c257
viniciusz1 <vinicius101313@gmail.com>
2022-03-31 09:29:13 -0300

    ARRUMANDO LOGIN

--- Imobiliaria/src/app/telas/login/tela-cadastro/tela-cadastro.component.ts
+++ Imobiliaria/src/app/telas/login/tela-cadastro/tela-cadastro.component.ts
@@ -19,11 +19,19 @@ export class TelaCadastroComponent implements OnInit {
   pass = ""
   pass2 = "";
   adm = false;
+  quantidade = 0;
   cadastrar(){
     if(this.user != "" && this.pass != "" && this.pass2 != ""){
       if(this.pass2 == this.pass){
-        console.log("Igual")
         this.usuarioService.inserirUsuario(this.user, this.pass, this.adm)
+        this.usuarioService.buscarUsuarios().then(result => {
+          this.quantidade = (Object.keys(result).length)
+          this.quantidade = this.quantidade +1;
+          localStorage.setItem("ID", this.quantidade.toString())
+          localStorage.setItem("NOME", this.user);
+          localStorage.setItem("PASSWORD", this.pass);
+          localStorage.setItem("USER", this.user);
+        })
         this.router.navigate(['/compra'])
     }
     }
