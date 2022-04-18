a284e6d6da8af6304ab4c7dba55e8dc2d7431fbf
viniciusz1 <vinicius101313@gmail.com>
2022-04-18 19:21:36 -0300

    abc

--- basic-server/api/inserts.js
+++ basic-server/api/inserts.js
@@ -45,140 +45,140 @@ database(`INSERT INTO CLIENTE VALUES(
     "95355-5489",
     "108.423.265-37",
     "vinicius@gmail.com",
-    null,
+    "https://pps.whatsapp.net/v/t61.24694-24/178808345_2983340488551499_7326992364066034796_n.jpg?ccb=11-4&oh=5d058b320d03764b7266ac5dd99ccc89&oe=626BD84F",
     2019-05-05
 ),(
     "Thiago",
     "95355-5489",
     "302.323.245-87",
     "thiago@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14GgT3bxprX7vKeYgQUlz7zbLrqOYpylo_Fg_lI2_nw=s32-c",
     2019-05-05
 ),(
     "Henrique",
     "95355-5489",
     "133.423.245-87",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14GjsHk-XyGylNpzWSAjZfcV87HbquIq-UnqemkkjSQ=s32-c",
     2019-05-05  
 ),(
     "João",
     "95355-5489",
     "203.333.245-87",
     "henrique@gmail.com",
-    null,
+    "https://pps.whatsapp.net/v/t61.24694-24/260922947_1825204034345060_6949822982634661238_n.jpg?ccb=11-4&oh=3a9c371899737d6ad49134dc3252fbd8&oe=626DFD6E",
+    2019-05-05  
+),(
+    "Diego",
+    "95355-5489",
+    "222.333.245-87",
+    "henrique@gmail.com",
+    "https://lh3.googleusercontent.com/a-/AOh14Ggf1JXM-FMIua_IuQFG4sNNpdbk43GiNpJgAyxG=s32-c",
     2019-05-05  
 ),(
     "Otávio Augusto",
     "95355-5489",
     "303.323.245-87",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14Ggh6gGaDw0y94mek_IpkWI41GJibG7vOUUp2vwp=s32-c",
     2019-05-05  
 ),(
     "Otávio Neves",
     "95355-5489",
     "133.223.245-87",
     "henrique@gmail.com",
-    null,
+    "https://pps.whatsapp.net/v/t61.24694-24/185051966_202733028332894_7076629198708775680_n.jpg?ccb=11-4&oh=cd95462feb209ad375678c038cb840cd&oe=626E8CF9",
     2019-05-05  
 ),(
     "Camilly Pessoty",
     "95355-5489",
     "104.423.245-87",
     "henrique@gmail.com",
-    null,
+    "https://pps.whatsapp.net/v/t61.24694-24/233395459_1300280947143453_886180543505870274_n.jpg?ccb=11-4&oh=01_AVz-DbyVp2UlxMcebgojF34sJNj8bgk4ctv0XHDX2rDA7w&oe=626C9E18",
     2019-05-05  
 ),(
     "Camilly Vitória",
     "95355-5489",
     "123.443.245-87",
     "henrique@gmail.com",
-    null,
+    "https://pps.whatsapp.net/v/t61.24694-24/158135148_1107535780096864_8095936128135052549_n.jpg?ccb=11-4&oh=8b7940bfbb456d4a00526261ad3b7a5f&oe=626D0B87",
     2019-05-05  
 ),(
     "Vytor",
     "95355-5489",
     "203.423.244-87",
     "henrique@gmail.com",
-    null,
-    2019-05-05  
-),(
-    "Leonardo",
-    "95355-5489",
-    "103.423.255-81",
-    "henrique@gmail.com",
-    null,
+    "https://www.infoescola.com/wp-content/uploads/2008/07/sapo-561077704.jpg",
     2019-05-05  
 ),(
     "Kenzo",
     "95355-5489",
     "103.423.255-17",
     "henrique@gmail.com",
-    null,
+    "https://atacadominasdepresentes.com.br/media/catalog/product/cache/1/thumbnail/1200x1400/9df78eab33525d08d6e5fb8d27136e95/b/b/bbl1465s_2_.jpg",
     2019-05-05  
 ),(
     "Matheus",
     "95355-5489",
     "103.423.241-87",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14GgT3bxprX7vKeYgQUlz7zbLrqOYpylo_Fg_lI2_nw=s32-c",
     2019-05-05  
 ),(
     "Eduarda",
     "95355-5489",
     "103.426.215-87",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14GgGoManoO--jKaCDDwezTynuXkSIvFdjbpjjL4B=s32-c",
     2019-05-05  
 ),(
     "Ester",
     "95355-5489",
     "103.423.145-77",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14GgHXCEmwlXREO6GJe5pOEHMWkpqJeLd_d2aPQaX=s32-c",
     2019-05-05  
 ),(
     "Bruna",
     "95355-5489",
     "103.421.275-87",
     "henrique@gmail.com",
-    null,
+    "https://pps.whatsapp.net/v/t61.24694-24/217590065_1155376751968460_2490540237114280095_n.jpg?ccb=11-4&oh=01_AVwBtvmuOVZVPF6kISxPaXtUPz5_B04IvVe9_oqrZHYYiQ&oe=626E9679",
     2019-05-05  
 ),(
     "Felipe",
     "95355-5489",
     "103.413.745-87",
     "henrique@gmail.com",
-    null,
+    "https://atacadominasdepresentes.com.br/media/catalog/product/cache/1/thumbnail/1200x1400/9df78eab33525d08d6e5fb8d27136e95/b/b/bbl1465s_2_.jpg",
     2019-05-05  
 ),(
     "Leonardo Rafaelli",
     "95355-5489",
     "103.127.245-87",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14Gji8MN4kF_6zp7E4lstYFWole5QuAmUn5qeLV2Q=s32-c",
     2019-05-05  
 ),(
     "Leonardo Poglia",
     "95355-5489",
     "101.473.245-87",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14GhEDFTEL04yo0wkq0yBP5DFjrbQBL6gnyQR5XnU=s32-c",
     2019-05-05  
 ),(
     "Gustavo",
     "95355-5489",
     "117.423.245-87",
     "henrique@gmail.com",
-    null,
+    "https://lh3.googleusercontent.com/a-/AOh14GjPV2qp9W5vMdk92vQhSkrxoE-MMRhQlecJIX-h=s32-c",
     2019-05-05  
 ),(
     "Bruno Henrique",
     "95355-5489",
     "113.427.245-87",
     "henrique@gmail.com",
-    null,
+    "https://avatars.githubusercontent.com/u/19558449?v=4",
     2019-05-05  
 )`).then(result => {
     console.log('insert cliente deu boa!')
